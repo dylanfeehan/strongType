f3baf28060d645d699c486363f10277ec12ee1d2
dylanfeehan <dylanfeehan981@gmail.com>
2021-09-06 15:10:40 -0400

    fixing time

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-window.addEventListener('load', init)
+window.addEventListener('load', init);
 const timeDisplay = document.querySelector('#time');
+let time = 0;
+//sconst words = ["for", "each", "while", "java", "python", "int", "boolean", "double", "javascript"];
+//const wordsDisplay = document.querySelector('#words');
+
 function init(){
 	setInterval(countUp, 1000);
+	//wordsDisplay.innerHTML = words[0];
 }
 function countUp(){
 	time++;
